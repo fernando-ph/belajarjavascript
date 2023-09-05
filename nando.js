8ecadc1590480eded0b02961e6803f3129d60c13
fernando-ph <fernandolumbangaol11@gmail.com>
2023-09-05 13:42:59 +0700

    ahskalljhbsnksk

--- nando.js
+++ nando.js
@@ -22,3 +22,30 @@ let result=`${1+3}`
 console.log(result)//ini untuk backtik
 console.log(`${result} yok ${tuti} tambah ${9*7}`)
 console.log(sayang1)
+
+
+//tipe data bolean adalah logika antara true dan false
+//tipe data null tipe data yang berisi data kosong
+//example
+let bilangansatu=8
+let bilangandua=9
+let kosong=null
+let kosongdua=
+console.log( bilangansatu>bilangandua ) //false karena 8 kecil 
+console.log( bilangandua>bilangansatu ) //true karena 9 besar
+console.log(kosong)//null atau kosong
+console.log(kosongdua)//null atau undfine tidak di defensikan 
+//tipe data primitif itu int bolean char float
+//tipe data non primitif array obejkdan lalin lain
+
+const babi={
+nama:"shs",
+kelas:12,
+bool:true,
+say:`${9*7}`
+
+}
+
+console.log(babi)
+
+
