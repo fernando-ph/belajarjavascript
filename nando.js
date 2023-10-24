189ebdd740f0ffb1a8b4104b1992fde55602195f
fernando-ph <fernandolumbangaol11@gmail.com>
2023-10-25 04:15:31 +0700

    shddf

--- nando.js
+++ nando.js
@@ -1,35 +1,7 @@
-//penggunan if adalah untuk membutat logika atau kondisi jika terpenuhi maka jalan jika tidak maka akan jalan dikonsis alain
-let nando=12
-
-if(nando==12){
-    console.log(nando+12)//ini adalah if aja 
-}
-else{
-    console.log("input salah ")
-}
-
-let gaol=-1
-if(gaol>=20){//jika konisi pertama terpenuhi dia akan masuk ke kondisi 2 
-    if(nando %2==0){
-        console.log(nando+gaol)//ini adalah if didalam if alias if bersarang
-
-    }else if(19<gaol){
-        console.log("gak ")
-
-    }else{
-        console.log("ulang")
-
-    }
-
-
-}else{//jika kondisi tidak terpenuhui maka masuk ke sini
-console.log("input salah")
-}
-
-
-///menjalan logika di dalam variavble
-
-let bunda=12
-let a=bunda==13
-console.log(a)//a sebagai variable penampung dan bunda sebaagi pembanding dengan 13 jadi jika dia bunda sama dengan 13
-//akan true tapi dai salah dia cuma 12 makanya false
+// let nanod=12
+// let x=90
+// for i in range(j){
+    
+// }
+const p=23
+console.log(p)
